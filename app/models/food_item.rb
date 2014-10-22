2f7dc0928594cec39ef440b8449fc2562c83ecdc
Harry Ng <harryworld@gmail.com>
2014-10-22 16:40:43 +0800

    Add image uploader

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,2 +1,3 @@
 class FoodItem < ActiveRecord::Base
+  mount_uploader :picture, PictureUploader
 end
--- spec/features/upload_image_spec.rb
+++ spec/features/upload_image_spec.rb
@@ -6,7 +6,16 @@ feature 'Visit the image uploader page' do
     expect(page).to have_content 'Image Uploader'
   end
 
-  scenario 'Upload Successful'
+  scenario 'Upload Successful' do
+    visit new_food_item_path
+
+    fill_in 'Name', :with => 'Chicken'
+    click_button 'Create Food item'
+    expect(page).to have_content 'Food item was successfully created.'
+    expect(page).to have_content 'Chicken'
+
+    expect(page).to have_content 'Picture'
+  end
 
   scenario 'Show the image'
 end
