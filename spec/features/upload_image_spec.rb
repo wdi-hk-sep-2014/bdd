6afe2819d91d30c712cfd2a2678622fded6ef08e
Harry Ng <harryworld@gmail.com>
2014-10-22 12:43:39 +0800

    Change the content

--- spec/features/upload_image_spec.rb
+++ spec/features/upload_image_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Visit the image uploader page' do
   scenario 'Upload Image' do
     visit '/'
-    expect(page).to have_content 'Image'
+    expect(page).to have_content 'Image Uploader'
   end
 end
